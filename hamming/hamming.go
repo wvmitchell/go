88d93aa62e935e672fdc7ded965784f05e2bc448
Will Mitchell <wvmitchell@gmail.com>
2013-12-23 11:29:56 -0500

    Refactored hamming to eliminate helper function and shorten naming conventions

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,29 +1,21 @@
 package hamming
 
-import (
-	"strings"
-)
+import "strings"
 
-func Distance(first_sequence, second_sequence string) int {
-	first_letters := strings.Split(first_sequence, "")
-	second_letters := strings.Split(second_sequence, "")
-	return difference(first_letters, second_letters)
+func Distance(sequence1, sequence2 string) int {
+	s1 := strings.Split(sequence1, "")
+	s2 := strings.Split(sequence2, "")
+	return difference(s1, s2)
 }
 
-func difference(first, second []string) int {
-	count := 0
-	for i := range shorter(first, second) {
-		if first[i] != second[i] {
+func difference(s1, s2 []string) (count int) {
+	if len(s1) > len(s2) {
+		s1, s2 = s2, s1
+	}
+	for i := range s1 {
+		if s1[i] != s2[i] {
 			count++
 		}
 	}
-	return count
-}
-
-func shorter(first, second []string) []string {
-	if len(first) > len(second) {
-		return second
-	} else {
-		return first
-	}
+	return
 }
---
+++ word-count/word_count.go
@@ -0,0 +1,7 @@
+package wc
+
+Histogram struct{
+}
+
+func WordCount(){
+}
