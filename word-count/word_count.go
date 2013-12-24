1329e0e6dc1afc377dfc713183753e192b9eab30
Will Mitchell <wvmitchell@gmail.com>
2013-12-24 13:58:49 -0500

    Go wordcount in progress

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,6 +1,20 @@
 package wc
 
-Histogram struct{
+import "reflect"
+
+type Histogram struct{
+  word_counts map[string]int
+}
+
+func NewHistogram(word_counts map[string]int) (h *Histogram) {
+  for word, count := range word_counts {
+    h.word_counts[word] = count
+  }
+  return
+}
+
+func (h Histogram) Equal(other Histogram) bool {
+  return reflect.DeepEqual(h.word_counts, other.word_counts)
 }
 
 func WordCount(){
