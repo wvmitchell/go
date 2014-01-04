fb7896994764ebd22162dfc5d2f42773720f9513
Will Mitchell <wvmitchell@gmail.com>
2014-01-04 13:37:33 -0700

    Changes to word-count

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,26 +1,28 @@
 package wc
 
 import (
-  "reflect"
-  "strings"
+	"reflect"
+	"strings"
+  "regexp"
 )
 
 type Histogram map[string]int
 
 func (h Histogram) Equal(other Histogram) bool {
-  return reflect.DeepEqual(h, other)
+	return reflect.DeepEqual(h, other)
 }
 
 func WordCount(words string) Histogram {
-  h := make(Histogram)
-  populateHistogram(h, words)
-  return h
+	h := make(Histogram)
+	populateHistogram(h, words)
+	return h
 }
 
 func populateHistogram(h Histogram, words string) {
-  split_words := strings.Split(words, " ")
-  for i := range split_words {
-    h[split_words[i]] += 1
-  }
+  re := regexp.MustCompile(/\W*/)
+  cleaned := re.ReplaceAllString(words, "")
+	split_words := strings.Split(words, " ")
+	for i := range split_words {
+		h[split_words[i]] += 1
+	}
 }
-
