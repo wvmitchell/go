4c51af5fee1917b1214441f9881ef62ce9d9656b
William Mitchell <wvmitchell@gmail.com>
2014-03-26 16:03:53 -0600

    discovered the ParseInt method!

--- octal/octal.go
+++ octal/octal.go
@@ -1,30 +1,10 @@
 package octal
 
 import (
-	"math"
 	"strconv"
-	"strings"
 )
 
 func ToDecimal(octaldecimal string) int64 {
-	return convertDigits(octaldecimal)
-}
-
-func convertDigits(octaldecimal string) int64 {
-	result := float64(0)
-	digits := strings.Split(reverse(octaldecimal), "")
-
-	for index, digit := range digits {
-		as_int, _ := strconv.Atoi(digit)
-		result += float64(as_int) * math.Pow(float64(8), float64(index))
-	}
-	return int64(result)
-}
-
-func reverse(octaldecimal string) string {
-	runes := []rune(octaldecimal)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
+  n, _ := strconv.ParseInt(octaldecimal, 8, 64)
+  return n
 }
