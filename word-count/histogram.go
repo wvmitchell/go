ab698a6a8c5103b6edc3bc75590ec0743db25709
William Mitchell <wvmitchell@gmail.com>
2014-03-26 11:43:27 -0600

    historgram populates, still needs cleaned string

--- word-count/histogram.go
+++ word-count/histogram.go
@@ -1,3 +1,31 @@
 package wc
 
+import (
+	"reflect"
+	"strings"
+  //"regexp"
+)
+
 type Histogram map[string]int
+
+func (h Histogram) Equal(other Histogram) bool {
+	return reflect.DeepEqual(h, other)
+}
+
+func WordCount(words string) Histogram {
+	h := make(Histogram)
+  cleaned := cleanWords(words)
+	populateHistogram(h, cleaned)
+	return h
+}
+
+func populateHistogram(h Histogram, words string) {
+  words_collection := strings.Split(words, " ")
+  for _, word := range words_collection {
+    h[word] += 1
+  }
+}
+
+func cleanWords(words string) string {
+  return words
+}
