daade0c823c60df6ef5127b1cff2fa3472617144
William Mitchell <wvmitchell@gmail.com>
2014-03-26 15:37:33 -0600

    ran fmt

--- octal/octal.go
+++ octal/octal.go
@@ -1,30 +1,30 @@
 package octal
 
 import (
-  "strconv"
-  "strings"
-  "math"
+	"math"
+	"strconv"
+	"strings"
 )
 
 func ToDecimal(octaldecimal string) int64 {
-  return convertDigits(octaldecimal)
+	return convertDigits(octaldecimal)
 }
 
 func convertDigits(octaldecimal string) int64 {
-  result := float64(0)
-  digits := strings.Split(reverse(octaldecimal), "")
+	result := float64(0)
+	digits := strings.Split(reverse(octaldecimal), "")
 
-  for index, digit := range digits {
-    as_int, _ := strconv.Atoi(digit)
-    result += float64(as_int) * math.Pow(float64(8), float64(index))
-  }
-  return int64(result)
+	for index, digit := range digits {
+		as_int, _ := strconv.Atoi(digit)
+		result += float64(as_int) * math.Pow(float64(8), float64(index))
+	}
+	return int64(result)
 }
 
 func reverse(octaldecimal string) string {
-  runes := []rune(octaldecimal)
-  for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-    runes[i], runes[j] = runes[j], runes[i]
-  }
-  return string(runes)
+	runes := []rune(octaldecimal)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
